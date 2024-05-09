48fcd1e04be101e6338055df85ecc5737f952d07
AndreusLara <148370729+AndreusLara@users.noreply.github.com>
2024-05-09 12:57:19 -0400

    Update DamageIcon.cs

--- Scripts/DamageIcon.cs
+++ Scripts/DamageIcon.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class DamageIcon : MonoBehaviour
 {
-
+// imports sprite for damage icons
     public Sprite[] damageSprites;
 
 	public GameObject effect;
@@ -18,11 +18,11 @@ public class DamageIcon : MonoBehaviour
 		camAnim = Camera.main.GetComponent<Animator>();
 		camAnim.SetTrigger("shake");
 	}
-
+// render the textmesh pro on the damage icon 
 	public void Setup(int damage) {
         GetComponent<SpriteRenderer>().sprite = damageSprites[damage - 1];
     }
-
+// destroys the damage sprite object
 	void Destruction(){
 		Instantiate(effect, transform.position, Quaternion.identity);
 		Destroy(gameObject);
